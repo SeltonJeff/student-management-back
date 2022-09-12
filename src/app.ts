3ae17906bcd75bd78a82bc554c4e1aedffb7ca2e
Selton Jeffeson <selton.jeffeson@catskillet.com>
2022-09-12 08:48:14 -0300

    refactor: asjuts on initialize DB, server and app

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,12 @@ import express, { Application } from "express";
 import * as bodyParser from "body-parser";
 import cors from "cors";
 import AppRoutes from "./routes";
-import dataSource from "./dataSource";
-import authServices from "./services/authServices";
 import swaggerRoutes from "./swaggerRoutes";
 
 class App {
   public application: Application;
 
   constructor() {
-    this.useDatabase();
     this.application = express();
     this.useMiddlewares();
   }
@@ -21,16 +18,6 @@ class App {
     this.application.use(cors());
     this.application.use(AppRoutes);
     this.application.use(await swaggerRoutes.load());
-  }
-
-  private useDatabase() {
-    dataSource
-      .initialize()
-      .then(() => {
-        console.log("db initialized.");
-        authServices.registerAminUser();
-      })
-      .catch((error) => console.log("db init failed.", error));
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import App from "./app";
 import env from "./env";
+import dataSource from "./dataSource";
+import authServices from "./services/authServices";
 
 class Server {
   public readonly app = App;
@@ -9,9 +11,15 @@ class Server {
   }
 
   private startServer() {
-    this.app.listen(env.APP_PORT, () =>
-      console.log("App running at: ", `http://localhost:${env.APP_PORT}`)
-    );
+    dataSource
+      .initialize()
+      .then(() => {
+        authServices.registerAminUser();
+        this.app.listen(env.APP_PORT, () =>
+          console.log("App running at: ", `http://localhost:${env.APP_PORT}`)
+        );
+      })
+      .catch(() => console.log("Failed connect to DB."));
   }
 }
 
